9b698aa0d0378aa97ca10383c1b21a4f79172d7c
Andrew Chien <aechien@gmail.com>
2020-03-10 12:39:32 -0400

    Refactor interface threads
    Split into a few different threads and structs instead of one big
    function.  Define the communication between them better.

--- src/interface.rs
+++ src/interface.rs
@@ -6,161 +6,231 @@ use std::io::{self, Write};
 use std::str::SplitWhitespace;
 use std::thread;
 
-pub fn run() {
-  let movegen = MoveGenerator::new();
-  let mut position: Option<Position> = None;
+struct Interface {
+  movegen: MoveGenerator,
 
-  let (send_stdout, recv_stdout) = crossbeam_channel::unbounded();
-  let (send_stderr, recv_stderr) = crossbeam_channel::unbounded();
-  let (send_quit_output, recv_quit_output) = crossbeam_channel::bounded(1);
-  let (send_quit_search, recv_quit_search) = crossbeam_channel::bounded(1);
+  output_thread: OutputThread,
+  search_thread: Option<SearchThread>,
+}
 
-  let output_thread = thread::spawn(move || loop {
-    select! {
-      recv(recv_stdout) -> s => {
-        println!("{}", s.unwrap());
-        io::stdout().flush().unwrap();
-      }
-      recv(recv_stderr) -> s => {
-        eprintln!("{}", s.unwrap());
-        io::stderr().flush().unwrap();
-      }
-      recv(recv_quit_output) -> _ => {
-        return;
-      }
-    }
-  });
-  let mut search_thread: Option<thread::JoinHandle<()>> = None;
+struct OutputThread {
+  handle: thread::JoinHandle<()>,
+  send_quit: crossbeam_channel::Sender<()>,
+  output: Output,
+}
 
-  let not_implemented = |line: &str| {
-    send_stderr.send(format!("not implemented: {}", line.trim())).unwrap();
-  };
+#[derive(Clone)]
+struct Output {
+  send_stdout: crossbeam_channel::Sender<String>,
+  send_stderr: crossbeam_channel::Sender<String>,
+}
 
-  loop {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
+struct SearchThread {
+  handle: thread::JoinHandle<()>,
+  send_quit: crossbeam_channel::Sender<()>,
+}
 
-    let mut tokens = line.split_whitespace();
-    let command = tokens.next();
-    match command {
-      None => continue,
-      Some("uci") => {
-        send_stdout
-          .send(format!(
-            "id name Chessier v{}\nid author Andrew Chien",
-            env!("CARGO_PKG_VERSION")
-          ))
-          .unwrap();
-        send_stdout.send(String::from("uciok")).unwrap();
-      }
-      Some("debug") => not_implemented(&line),
-      Some("isready") => send_stdout.send(String::from("readyok")).unwrap(),
-      Some("setoption") => not_implemented(&line),
-      Some("register") => not_implemented(&line),
-      Some("ucinewgame") => (),
-      Some("position") => match parse_position(&movegen, &mut tokens) {
-        Ok(pos) => position = Some(pos),
-        Err(msg) => send_stderr.send(msg).unwrap(),
-      },
-      Some("go") => match &mut position {
-        None => send_stderr
-          .send(String::from("no position provided before 'go'"))
-          .unwrap(),
-        Some(position) => {
-          if let Some(search_thread) = search_thread {
-            send_quit_search.send(()).unwrap();
-            search_thread.join().unwrap();
-            // Clear the signal, because the search thread may already have
-            // terminated and not needed the signal.
-            recv_quit_search.try_recv().unwrap_or(());
-          }
-          let mut position = position.clone();
-          let send_stdout = send_stdout.clone();
-          let recv_quit_search = recv_quit_search.clone();
-          search_thread = Some(thread::spawn(move || {
-            let mut search = Search::new(Some(recv_quit_search));
-            match search.search(&mut position, 8) {
-              SearchResult::Abort => (),
-              SearchResult::Fail(_) => {
-                panic!("searched got pruned at top level: {:?}", position);
-              }
-              SearchResult::Success(score, m) => {
-                send_stdout.send(format!("info score cp {}", score)).unwrap();
-                send_stdout
-                  .send(format!("bestmove {}", m.unwrap().long_algebraic()))
-                  .unwrap();
-              }
-            };
-          }));
+impl OutputThread {
+  pub fn spawn() -> Self {
+    let (send_stdout, recv_stdout) = crossbeam_channel::unbounded();
+    let (send_stderr, recv_stderr) = crossbeam_channel::unbounded();
+    let (send_quit, recv_quit) = crossbeam_channel::bounded(1);
+
+    let handle = thread::spawn(move || loop {
+      select! {
+        recv(recv_stdout) -> s => {
+          println!("{}", s.unwrap());
+          io::stdout().flush().unwrap();
         }
-      },
-      Some("stop") => not_implemented(&line),
-      Some("ponderhit") => not_implemented(&line),
-      Some("quit") => {
-        send_quit_output.send(()).unwrap();
-        if let Some(search_thread) = search_thread {
-          send_quit_search.send(()).unwrap();
-          search_thread.join().unwrap();
+        recv(recv_stderr) -> s => {
+          eprintln!("{}", s.unwrap());
+          io::stderr().flush().unwrap();
         }
-        output_thread.join().unwrap();
-        return;
+        recv(recv_quit) -> _ => {
+          return;
+        }
       }
-      Some(_) => {
-        send_stderr.send(format!("Unknown command: {}", line.trim())).unwrap()
-      }
-    }
+    });
+
+    Self { handle, send_quit, output: Output { send_stdout, send_stderr } }
+  }
+
+  fn quit(self) {
+    self.send_quit.send(()).unwrap();
+    self.handle.join().unwrap();
   }
 }
 
-fn parse_position(
-  movegen: &MoveGenerator,
-  tokens: &mut SplitWhitespace,
-) -> Result<Position, String> {
-  let mut position: Position;
-  match tokens.next() {
-    Some("startpos") => {
-      position = Position::startpos();
-      match tokens.next() {
-        Some("moves") => (),
-        Some(t) => {
-          return Err(format!(
-            "unexpected token \"{}\" after \"position startpos\"",
-            t,
-          ))
-        }
-        None => return Ok(position),
-      }
-    }
-    Some("fen") => {
-      let mut fen = String::new();
-      loop {
-        match tokens.next() {
-          Some("moves") => break,
-          Some(fen_part) => {
-            fen.push_str(fen_part);
-            fen.push_str(" ");
-          }
-          None => break,
-        }
-      }
-      match Position::from_fen(&fen) {
-        Ok(pos) => position = pos,
-        Err(e) => {
-          return Err(format!("Error parsing fen \"{}\": {:?}", fen, e))
-        }
-      }
-    }
-    _ => return Err(String::from("position needs startpos or fen")),
+impl Output {
+  pub fn println(&self, msg: &str) {
+    self.send_stdout.send(msg.into()).unwrap();
   }
 
-  for algebraic_move in tokens {
-    let m = match movegen.parse_move(&position, algebraic_move) {
-      Ok(mv) => mv,
-      Err(e) => {
-        return Err(format!("Error parsing move {}: {:?}", algebraic_move, e))
-      }
-    };
-    position.make_move(m);
+  pub fn eprintln(&self, msg: &str) {
+    self.send_stdout.send(msg.into()).unwrap();
   }
-  Ok(position)
+}
+
+impl SearchThread {
+  pub fn spawn(position: Position, output: Output) -> Self {
+    let (send_quit, recv_quit) = crossbeam_channel::bounded(1);
+    let handle = thread::spawn(move || {
+      let mut position = position;
+      let mut search = Search::new(Some(recv_quit));
+      match search.search(&mut position, 8) {
+        SearchResult::Abort => (),
+        SearchResult::Fail(_) => {
+          panic!("searched got pruned at top level: {:?}", position);
+        }
+        SearchResult::Success(score, m) => {
+          output.println(&format!("info score cp {}", score));
+          output.println(&format!("bestmove {}", m.unwrap().long_algebraic()));
+        }
+      };
+    });
+
+    Self { handle, send_quit }
+  }
+
+  pub fn quit(self) {
+    // Send quit and ignore the result.  We don't unwrap because this can
+    // be an error if the search thread already finished becuase it disconnects
+    // the channel when all receivers are closed.
+    let _ = self.send_quit.send(());
+    self.handle.join().unwrap();
+  }
+}
+
+impl Interface {
+  fn loop_input(mut self) {
+    let output = &self.output_thread.output;
+    let mut position: Option<Position> = None;
+    loop {
+      let mut line = String::new();
+      let bytes_read = io::stdin().read_line(&mut line).unwrap();
+      if bytes_read == 0 {
+        // EOF
+        self.quit();
+        return;
+      }
+      let mut tokens = line.split_whitespace();
+      let command = tokens.next();
+      match command {
+        None => continue,
+        Some("uci") => {
+          output.println(&format!(
+            "id name Chessier v{}\nid author Andrew Chien",
+            env!("CARGO_PKG_VERSION")
+          ));
+          output.println("uciok");
+        }
+        Some("debug") => self.not_implemented(&line),
+        Some("isready") => output.println("readyok"),
+        Some("setoption") => self.not_implemented(&line),
+        Some("register") => self.not_implemented(&line),
+        Some("ucinewgame") => (),
+        Some("position") => match self.parse_position(&mut tokens) {
+          Ok(pos) => position = Some(pos),
+          Err(msg) => output.eprintln(&msg),
+        },
+        Some("go") => match &mut position {
+          None => output.eprintln("no position provided before 'go'"),
+          Some(position) => {
+            if let Some(search_thread) = self.search_thread {
+              search_thread.quit();
+            }
+            self.search_thread =
+              Some(SearchThread::spawn(position.clone(), output.clone()));
+          }
+        },
+        Some("stop") => self.not_implemented(&line),
+        Some("ponderhit") => self.not_implemented(&line),
+        Some("quit") => {
+          self.quit();
+          return;
+        }
+        Some(_) => {
+          output.eprintln(&format!("Unknown command: {}", line.trim()))
+        }
+      }
+    }
+  }
+
+  fn not_implemented(&self, line: &str) {
+    self
+      .output_thread
+      .output
+      .eprintln(&format!("not implemented: {}", line.trim()));
+  }
+
+  fn quit(self) {
+    self.output_thread.quit();
+    if let Some(search_thread) = self.search_thread {
+      search_thread.quit();
+    }
+  }
+
+  fn parse_position(
+    &self,
+    tokens: &mut SplitWhitespace,
+  ) -> Result<Position, String> {
+    let mut position: Position;
+    match tokens.next() {
+      Some("startpos") => {
+        position = Position::startpos();
+        match tokens.next() {
+          Some("moves") => (),
+          Some(t) => {
+            return Err(format!(
+              "unexpected token \"{}\" after \"position startpos\"",
+              t,
+            ))
+          }
+          None => return Ok(position),
+        }
+      }
+      Some("fen") => {
+        let mut fen = String::new();
+        loop {
+          match tokens.next() {
+            Some("moves") => break,
+            Some(fen_part) => {
+              fen.push_str(fen_part);
+              fen.push_str(" ");
+            }
+            None => break,
+          }
+        }
+        match Position::from_fen(&fen) {
+          Ok(pos) => position = pos,
+          Err(e) => {
+            return Err(format!("Error parsing fen \"{}\": {:?}", fen, e))
+          }
+        }
+      }
+      _ => return Err(String::from("position needs startpos or fen")),
+    }
+    for algebraic_move in tokens {
+      let m = match self.movegen.parse_move(&position, algebraic_move) {
+        Ok(mv) => mv,
+        Err(e) => {
+          return Err(format!(
+            "Error parsing move {}: {:?}",
+            algebraic_move, e
+          ))
+        }
+      };
+      position.make_move(m);
+    }
+    Ok(position)
+  }
+}
+
+pub fn run() {
+  let interface = Interface {
+    movegen: MoveGenerator::new(),
+    output_thread: OutputThread::spawn(),
+    search_thread: None,
+  };
+  interface.loop_input();
 }
